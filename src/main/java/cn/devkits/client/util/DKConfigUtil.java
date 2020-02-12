e70c59799e02c9472bb116a0d95330ffbcc24408
zhaoxiaojuan <liushaofeng89@qq.com>
2020-02-12 22:40:17 +0800

    修改调试环境下，读取配置文件的方式

--- src/main/java/cn/devkits/client/util/DKConfigUtil.java
+++ src/main/java/cn/devkits/client/util/DKConfigUtil.java
@@ -20,21 +20,17 @@ public class DKConfigUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(DKConfigUtil.class);
 
     private static DKConfigUtil INSTANCE = new DKConfigUtil();
-    private Properties cfgProperties = new Properties();
     private Model model;
 
     private DKConfigUtil() {
-        try {
-            cfgProperties.load(DKConfigUtil.class.getResourceAsStream("/META-INF/maven/cn.devkits.client/devkits/pom.properties"));
-        } catch (NullPointerException e) {
-            LOGGER.error("Load devkits pom.properties file failed: " + e.getMessage());
-        } catch (IOException e) {
-            LOGGER.error("Load devkits pom.xml file failed: " + e.getMessage());
-        }
+        loadVersionProperties(DKSystemUtil.isRunWithJar());
+    }
 
+    private void loadVersionProperties(boolean isRunWithJar) {
+        String pom = isRunWithJar ? "/META-INF/maven/cn.devkits.client/devkits/pom.xml" : "/pom.xml";
         MavenXpp3Reader reader = new MavenXpp3Reader();
         try {
-            model = reader.read(DKConfigUtil.class.getResourceAsStream("/META-INF/maven/cn.devkits.client/devkits/pom.xml"));
+            model = reader.read(DKConfigUtil.class.getResourceAsStream(pom));
         } catch (IOException | XmlPullParserException e) {
             LOGGER.error("Load pom.xml file failed: " + e.getMessage());
         }
@@ -48,9 +44,6 @@ public class DKConfigUtil {
         return model;
     }
 
-    public Properties getPomProperties() {
-        return cfgProperties;
-    }
 
     public String getAboutHtml() {
         if (model == null) {
@@ -63,8 +56,6 @@ public class DKConfigUtil {
         sb.append("Devkits is a toolkit for improving work efficiency.");
         sb.append("<br/><br/>");
         sb.append("Author: shaofeng liu");
-        sb.append("<br/>");
-        sb.append("BuildTime: 20190812120334");
         sb.append("<br/>");
         sb.append("HomePage: <a href='");
         sb.append(model.getUrl());
@@ -79,6 +70,11 @@ public class DKConfigUtil {
 
     public String getIssueUri() {
         return model.getIssueManagement().getUrl();
+    }
+
+
+    public String getVersion() {
+        return model.getVersion();
     }
 
 }
