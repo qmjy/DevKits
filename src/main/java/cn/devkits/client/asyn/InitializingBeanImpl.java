79e2676d103c17c1e206c75537d623f923ab54de
liushaofeng-hp <liushaofeng89@qq.com>
2021-12-15 21:17:49 +0800

    优化tray ico 图标刷新功能

--- src/main/java/cn/devkits/client/asyn/InitializingBeanImpl.java
+++ src/main/java/cn/devkits/client/asyn/InitializingBeanImpl.java
@@ -1,21 +1,30 @@
 package cn.devkits.client.asyn;
 
+import cn.devkits.client.App;
+import cn.devkits.client.mapper.BaseMapper;
+import cn.devkits.client.model.SysConfig;
+import cn.devkits.client.service.SysConfigService;
+import cn.devkits.client.util.DKSystemUIUtil;
+
+import com.sun.management.OperatingSystemMXBean;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cn.devkits.client.App;
-import cn.devkits.client.mapper.BaseMapper;
-import cn.devkits.client.model.SysConfig;
-import cn.devkits.client.service.SysConfigService;
+import oshi.hardware.CentralProcessor;
 
-import javax.swing.ImageIcon;
-import java.awt.Image;
-import java.awt.TrayIcon;
+import javax.swing.*;
+import java.awt.*;
+import java.lang.management.ManagementFactory;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -42,7 +51,13 @@ public class InitializingBeanImpl implements InitializingBean {
     }
 
     private void updateTrayIcon() {
-        new Thread(new TrayIconUpdateThread()).start();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new Thread(new TrayIconUpdateThread()).start();
+            }
+        }, 1500);
+
     }
 
     private void initUuid() {
@@ -62,8 +77,14 @@ public class InitializingBeanImpl implements InitializingBean {
         baseDao.createEmailTable();
     }
 
+    /**
+     * Tray Icon更新线程
+     */
     class TrayIconUpdateThread extends Thread {
         private final Logger LOGGER = LoggerFactory.getLogger(TrayIconUpdateThread.class);
+
+        private long[] oldTicks = new long[CentralProcessor.TickType.values().length];
+        private Map<String, Image> icoMap = new HashMap<>();
 
         public TrayIconUpdateThread() {
             super("DK-Tray-Icon-Update-Thread");
@@ -71,19 +92,40 @@ public class InitializingBeanImpl implements InitializingBean {
 
         @Override
         public void run() {
+            TrayIcon trayIcon = App.getTrayIcon();
+
+            int index = 0;
             while (true) {
-                URL resource = App.class.getClassLoader().getResource("assets\\trayico\\dark_cat_0.png");
-                Image image = new ImageIcon(resource).getImage();
-                TrayIcon trayIcon = App.getTrayIcon();
+                String imgUrl = "assets/trayicon/dark_cat_" + index + ".png";
+
+                Image image = icoMap.get(imgUrl);
+                if (image == null) {
+                    URL resource = TrayIconUpdateThread.class.getClassLoader().getResource(imgUrl);
+                    image = new ImageIcon(resource).getImage();
+                    icoMap.put(imgUrl, image);
+                }
+
+                int percentCpuLoad = getCpuLoad();
                 if (trayIcon != null) {
+                    trayIcon.setToolTip(DKSystemUIUtil.getLocaleStringWithParam("TRAY_ICON_MSG", percentCpuLoad));
                     trayIcon.setImage(image);
                 }
                 try {
-                    TimeUnit.MILLISECONDS.sleep(100);
+                    index++;
+                    if (index % 4 == 0) {
+                        index = 0;
+                    }
+                    TimeUnit.MILLISECONDS.sleep(100 - percentCpuLoad);
                 } catch (InterruptedException e) {
-                    LOGGER.error("DK-Tray-Icon-Update-Thread Sleep failed!");
+                    LOGGER.error("DK-Tray-Icon-Update-Thread sleep failed!");
                 }
             }
+        }
+
+        private int getCpuLoad() {
+            OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+            double systemCpuLoad = operatingSystemMXBean.getSystemCpuLoad();
+            return (int) (systemCpuLoad * 100);
         }
     }
 }
